b3906dfa6ab6732c2f760cf9764073c5aa78ce3d
EvilFinder <714251829@qq.com>
2016-06-02 15:34:45 +0800

    Update settings.py

--- fairy/settings.py
+++ fairy/settings.py
@@ -121,7 +121,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rootkitspace@gmail.com'
-EMAIL_HOST_PASSWORD = 'zhangyapeng'
+EMAIL_HOST_PASSWORD = ''
 
 # EMAIL_USE_TLS = True
 # EMAIL_HOST = 'smtp.exmail.qq.com'
